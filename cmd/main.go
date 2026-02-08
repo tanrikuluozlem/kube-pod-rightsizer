8ffe99a65e1c2219bc2a3d100dc27f2b24686bbe
Ozlem Tanrikulu <ozlemtanrikulu0@gmail.com>
2026-02-08 13:25:19 +0200

    Fix linter warnings for unchecked write errors

--- cmd/main.go
+++ cmd/main.go
@@ -92,12 +92,12 @@ func startHealthServer() {
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("ok"))
+		_, _ = w.Write([]byte("ok"))
 	})
 
 	mux.HandleFunc("/readyz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("ok"))
+		_, _ = w.Write([]byte("ok"))
 	})
 
 	server := &http.Server{
